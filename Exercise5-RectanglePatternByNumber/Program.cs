61bd5c6f752a93410e759c22abe6c85eae5520dc
Deepak Sharma <deepak.dev78@outlook.com>
2017-04-27 22:31:15 +1200

    - make rectangle using two for loops

--- Exercise5-RectanglePatternByNumber/Program.cs
+++ Exercise5-RectanglePatternByNumber/Program.cs
@@ -8,8 +8,31 @@ namespace Exercise5_RectanglePatternByNumber
 {
     class Program
     {
+        // Write a C# program that takes a number as input and then displays a rectangle of 3 columns wide and 5 rows tall using that digit.
+
         static void Main(string[] args)
         {
+            Console.Write("Enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (i == 0 || i == 4)
+                    {
+                        Console.Write(number);
+                    }
+                    else
+                    {
+                        j += j;
+                        Console.Write("{0} ",number);
+                    }
+                  
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
         }
     }
 }
